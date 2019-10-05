bf36d79bca8a4b484e32ec94daec424fead4a1f9
kento.horimoto <kentojason@gmail.com>
2019-10-05 14:16:03 +0000

    OVER UNDER気配の追加 成行気配の追加　データが""のときに気配barのwidthを0pxとする処理の追加 現在値と基準値の表示追加　気配barchartはひとまずこれで完了

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-//function onButtonClick() {
-//var kehaiquote = $("#input").val();
-//console.log(kehaiquote);
-//}
-
-
 var kehaiquote  = "6501/T";
   $("#button").click(function(){
       var kehaiquote = $("#input").val();
@@ -41,13 +35,17 @@ var kehaiquote  = "6501/T";
          url: kijunti_url,
          type:'GET',
          dataType:'json'
-      })
+      });
       //2つのAjaxのリクエストが終わったら実行する
          $.when(get_kehai, get_kijunti).done(function (kehai_result, kijunti_result) {
                         // console.log(kehai_result[0]) 
                           var cputime =kehai_result[0].cputime;
                           console.log("cputime:"+cputime);
-                            $(".kehaiquote").html("<p>" +kehaiquote+ "</p>");
+                          
+                             var genne = kijunti_result[0].section1.data[kehaiquote].DPP;
+                             var kijunti_k =kijunti_result[0].section1.data[kehaiquote].DSP;
+                            //TO:DO  現在価格と基準値を気配表示のようにNaNのとき""にする処理をいれる
+                            $(".kehaiquote").html("<p>" +kehaiquote+ "  現在価格  " +genne+ " 基準値 "+kijunti_k+ "</p>");
                             $("#main").html("<div>処理時間 "+cputime+"</div>");
                             kehaihyouji(kehai_result[0],kijunti_result[0],kehaiquote);
                     })
@@ -65,12 +63,12 @@ var kehaiquote  = "6501/T";
   });   
  
 
-  //気配表示　dataは気配APIの戻り値  data2は価格APIの戻り値 kehaiquoteはユーザーの入力値
+  //気配表示  dataは気配APIの戻り値  data2は価格APIの戻り値 kehaiquoteはユーザーの入力値
   function kehaihyouji(data,data2,kehaiquote) {
     $(".canvas").html("");
       if (data.section1.hitcount == 1){
     var width = 420; // グラフの幅
-    var height = 600; // グラフの高さ
+    var height = 690; // グラフの高さ
     
     //気配データの配列を用意
     var kehai=data.section1.data[kehaiquote];
@@ -127,22 +125,150 @@ var kehaiquote  = "6501/T";
 	    .domain([0, d3.max(Object.values(mkehai))])
 	    .range([0,180])
 	  //urixsclale  
-	  var urixscale =d3.scaleLinear()
+	var urixscale =d3.scaleLinear()
 	    .domain([0, d3.max(Object.values(mkehai))])
 	    .range([180,0])
 	 //overunderscale
   	 var ouxscale =d3.scaleLinear()
 	     .domain([0, d3.max(Object.values(oukehai))])
 	     .range([0,180])
-	   var narixscale =d3.scaleLinear()
+	 var narixscale =d3.scaleLinear()
 	     .domain([0, d3.max(Object.values(nariyukikehai))])
 	     .range([0,180])  
 	     
-	        
+	 //svgを用意       
      var svg= d3.select('.canvas')
               .append("svg")
                 .attr("width", width)
                 .attr("height", height)
+	 //固定文字を表示     
+	        svg.append('text')
+      	        .attr('x',205)
+                .attr('y',18)
+                .attr('text-anchor','middle')
+      	        .text('成行');
+      	    svg.append('text')
+      	        .attr('x',205)
+                .attr('y',48)
+                .attr('text-anchor','middle')
+      	        .text('OVER');
+      	     svg.append('text')
+      	        .attr('x',205)
+                .attr('y',678)
+                .attr('text-anchor','middle')
+      	        .text('UNDER');
+      	        
+     //売り成行気配・買い成行気配を表示
+            var g5=svg.selectAll('g')
+	            .data(Object.values(nariyukikehai))
+	              .enter();
+     
+            g5.append('rect')
+                .attr('fill','#DDCCEE')
+	            .attr('x',function(d,i) {
+	                if(i ==0){
+	                    //narixscaleは値が大きいほど大きいので、後の売り気配描画(urixscale＝値が大きいほど小さいとはwidthとｘの指定を逆にする。)
+	                     return 180-narixscale(d)+'px' ;
+	                }
+	                else{
+	                     return 235+'px';
+	                }
+	               
+                   })
+	            .attr('y',0)
+	            .attr('width', function(d) {
+	                if(d ==""){
+	                 return 0;
+	                 }
+	                else{ 
+	                 //narixscaleは値が大きいほど大きいので、後の売り気配描画(urixscale＝値が大きいほど小さいとはwidthとｘの指定を逆にする。)
+	                return narixscale(d)+'px' ;
+                   }
+	                
+	            })
+                .attr('height',29+'px');
+            g5.append('text')
+      	        .attr('x',function(d,i){
+      	             if(i ==0){
+	                     return 4+'px' ;
+	                }
+	                else{
+	                     return 360+'px';
+	                }  
+	                
+      	           })
+      	           
+                .attr('y',18)
+                .text(function(d){
+      	                return(d);
+      	           });
+ 
+     //OVER売り気配・UNDER買い気配を表示
+             var g6=svg.selectAll('g')
+	            .data(Object.values(oukehai))
+	              .enter();
+     
+            g6.append('rect')
+                .attr('fill',function(d,i) {
+	                if(i ==0){
+	                     return '#B6D9B5' ;
+	                }
+	                else{
+	                     return '#FFCCCC';
+	                }
+	               
+                   })
+	            .attr('x',function(d,i) {
+	                if(i ==0){
+	                     return 180-ouxscale(d)+'px' ;
+	                }
+	                else{
+	                     return 235+'px';
+	                }
+	               
+                   })
+	            .attr('y',function(d,i){
+      	             if(i ==0){
+	                     return 30+'px' ;
+	                }
+	                else{
+	                     return 660+'px';
+	                }
+	            })
+	                
+	            .attr('width', function(d) {
+	                  if(d ==""){
+	                 return 0;
+	                 }
+	                else{ 
+	                return ouxscale(d)+'px' ;
+	                }
+	               })
+	               
+                .attr('height',29+'px');
+            
+            g6.append('text')
+      	        .attr('x',function(d,i){
+      	             if(i ==0){
+	                     return 4+'px' ;
+	                }
+	                else{
+	                     return 360+'px';
+	                }  
+	                
+      	           })
+      	           
+	            .attr('y',function(d,i){
+      	             if(i ==0){
+	                     return 48+'px' ;
+	                }
+	                else{
+	                     return 678+'px';
+	                }
+	            })
+                .text(function(d){
+      	                return(d);
+      	           });    
 	   
 	   //買い気配の描画       
 	   var g=svg.selectAll('g')
@@ -153,18 +279,24 @@ var kehaiquote  = "6501/T";
       	         .attr('fill','#F9E3E1')
       	         .attr('x',235+'px')
       	         .attr('y',function(d,i){
-      	              return 300+30*i+'px';
+      	              return 360+30*i+'px';
       	                })
       	         .attr('width', function(d) {
+      	               if(d ==""){
+	                 return 0;
+	                 }
+	                else{ 
       	                return kaixscale(d) ;
-                         })
+	                 } 
+	                    
+	                })
                  .attr('height',29+'px')  	
               g.append('text')
                  .attr('x',function(d) {
-      	                return 370+'px';
+      	                return 360+'px';
                          })
                  .attr('y',function(d,i){
-      	            return 30*i+318+'px';
+      	            return 30*i+378+'px';
       	            })
       	         .attr('text-anchor','right')
       	         .text(function(d){
@@ -180,7 +312,7 @@ var kehaiquote  = "6501/T";
       	                return 205+'px';
                          })
                  .attr('y',function(d,i){
-      	            return 318+30*i+'px';
+      	            return 378+30*i+'px';
       	            })
       	         .attr('class',function(d){
       	           if(d > kijunti){
@@ -205,7 +337,7 @@ var kehaiquote  = "6501/T";
       	                return 205+'px';
                          })
                  .attr('y',function(d,i){
-      	            return 288-30*i+'px';
+      	            return 348-30*i+'px';
       	            })
       	          .attr('class',function(d){
       	           if(d > kijunti){
@@ -219,7 +351,8 @@ var kehaiquote  = "6501/T";
       	         .attr('text-anchor','middle')
       	         .text(function(d){
       	                return(d);
-      	           });	           
+      	           });
+
    
    //売り気配の描画   	           
     var g4=svg.selectAll('g4')
@@ -232,18 +365,22 @@ var kehaiquote  = "6501/T";
 	                return urixscale(d)+'px' ;
                    })
 	         .attr('y',function(d,i){
-	              return 268-30*i+'px';
+	              return 328-30*i+'px';
 	                })
 	         .attr('width', function(d) {
+	               if(d ==""){
+	                 return 0;
+	                 }
+	                else{ 
 	                return 180-urixscale(d)+'px' ;
-                   })
-           .attr('height',29+'px')  	
+	                }
+	                    
+	                })
+           .attr('height',29+'px') ;	
         g4.append('text')
-           .attr('x',function(d) {
-	                return 4+'px';
-                   })
+           .attr('x',4)
            .attr('y',function(d,i){
-	            return 288-30*i+'px';
+	            return 348-30*i+'px';
 	            })
 	         .text(function(d){
 	                return(d);
